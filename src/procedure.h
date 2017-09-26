cdf24dbab0af7aada4b2f281a6af8778b9d434e7
Skool17 <semen9422@gmail.com>
2017-09-26 22:28:32 +0700

    Head file

---
+++ src/procedure.h
@@ -0,0 +1,10 @@
+#ifndef OTHERFUNC_H
+#define OTHERFUNC_H
+
+float Discriminant(float a,float b,float c);
+
+int Proverka(float D);
+
+float Znachenie (float D, float a, float b, float c, int i);
+
+#endif
